7b1db4df6d2e9361f83123a57ee477b415216dad
Marija Djurdjevic <masadjurdjevic02@gmail.com>
2026-02-27 18:10:39 +0100

    modify knn

--- src/models.py
+++ src/models.py
@@ -1,17 +1,21 @@
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
+from sklearn.preprocessing import StandardScaler 
 
 def run_kmeans_baseline(df_encoded):
     """
     Pokreće K-Means klasterovanje (baseline) koristeći odabrane atribute.
+    Vrši obavezno skaliranje podataka pre klasterovanja.
     Vraća Silhouette rezultat (mjeru kvaliteta klastera).
     """
     X_cluster = df_encoded[['Caffeine_mg', 'Sleep_Hours']]
     
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X_cluster)
+    
     kmeans = KMeans(n_clusters=3, random_state=42, n_init=10)
+    klasteri = kmeans.fit_predict(X_scaled)
     
-    klasteri = kmeans.fit_predict(X_cluster)
-    
-    score = silhouette_score(X_cluster, klasteri)
+    score = silhouette_score(X_scaled, klasteri)
     
     return score
